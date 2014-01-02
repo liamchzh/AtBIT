c08f11b16ea46d8bc294292b5730bcfbfec89d04
liam <liamchzh@gmail.com>
2014-01-02 23:17:47 +0800

    Hole的数据表结构稍微改动，变成Holes

--- atbit.sql
+++ atbit.sql
@@ -1,31 +1,28 @@
--- phpMyAdmin SQL Dump
--- version 3.3.8.1
--- http://www.phpmyadmin.net
+-- MySQL dump 10.13  Distrib 5.5.34, for debian-linux-gnu (i686)
 --
--- 主机: w.rdc.sae.sina.com.cn:3307
--- 生成日期: 2013 年 10 月 27 日 00:24
--- 服务器版本: 5.5.27
--- PHP 版本: 5.3.3
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-
+-- Host: localhost    Database: app_saepy
+-- ------------------------------------------------------
+-- Server version	5.5.34-0ubuntu0.13.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- 数据库: `app_atbit`
+-- Table structure for table `Books`
 --
 
--- --------------------------------------------------------
-
---
--- 表的结构 `Books`
---
-
-CREATE TABLE IF NOT EXISTS `Books` (
+DROP TABLE IF EXISTS `Books`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Books` (
   `id` varchar(10) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `title` varchar(30) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `author` varchar(15) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
@@ -40,68 +37,96 @@ CREATE TABLE IF NOT EXISTS `Books` (
   `updatetime` datetime NOT NULL,
   UNIQUE KEY `id` (`id`,`isbn13`,`alt`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- 表的结构 `Buyers`
+-- Table structure for table `Buyers`
 --
 
-CREATE TABLE IF NOT EXISTS `Buyers` (
+DROP TABLE IF EXISTS `Buyers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Buyers` (
   `email` varchar(20) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `id` varchar(10) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `time` datetime NOT NULL,
   `active` varchar(1) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '1'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- 表的结构 `Collections`
+-- Table structure for table `Collections`
 --
 
-CREATE TABLE IF NOT EXISTS `Collections` (
+DROP TABLE IF EXISTS `Collections`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Collections` (
   `email` varchar(20) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `id` varchar(10) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `time` datetime NOT NULL,
   `active` varchar(1) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- 表的结构 `Hole`
+-- Table structure for table `Hole`
 --
 
-CREATE TABLE IF NOT EXISTS `Hole` (
+DROP TABLE IF EXISTS `Hole`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Hole` (
   `ip` varchar(20) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `user` varchar(20) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `name` varchar(10) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `content` varchar(250) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `time` datetime NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- 表的结构 `Messages`
+-- Table structure for table `Holes`
 --
 
-CREATE TABLE IF NOT EXISTS `Messages` (
+DROP TABLE IF EXISTS `Holes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Holes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ip` varchar(20) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `user` varchar(20) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(10) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `content` varchar(250) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `time` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `Messages`
+--
+
+DROP TABLE IF EXISTS `Messages`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Messages` (
   `fromid` varchar(8) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `toid` varchar(8) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `content` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `time` datetime NOT NULL,
   `read` varchar(1) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '0'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- 表的结构 `Sellers`
+-- Table structure for table `Sellers`
 --
 
-CREATE TABLE IF NOT EXISTS `Sellers` (
+DROP TABLE IF EXISTS `Sellers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Sellers` (
   `email` varchar(20) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `id` varchar(10) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `price` varchar(2) NOT NULL,
@@ -109,14 +134,16 @@ CREATE TABLE IF NOT EXISTS `Sellers` (
   `time` datetime NOT NULL,
   `active` varchar(1) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '1'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- 表的结构 `Users`
+-- Table structure for table `Users`
 --
 
-CREATE TABLE IF NOT EXISTS `Users` (
+DROP TABLE IF EXISTS `Users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Users` (
   `userid` varchar(8) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `email` varchar(20) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `password` varchar(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
@@ -128,3 +155,15 @@ CREATE TABLE IF NOT EXISTS `Users` (
   UNIQUE KEY `email` (`email`),
   UNIQUE KEY `userid` (`userid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2014-01-02 23:16:40
